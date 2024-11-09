02a79fc67d1c908aeb52f5b91f23dd226434764a
Erkinjon28091986 <info@jahonbozor.uz>
2024-11-10 02:39:25 +0500

    boshsahifa

--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,11 @@ $(".closelocation").on("click", function (event) {
 });
 
 //skeleton effect
-const skeletons = document.querySelectorAll('.skeleton') 
-skeletons.forEach((skeleton) => { 
-setTimeout(() => { 
-$(skeletons).removeClass('skeleton')
- }, 2000) 
+const skeletons = document.querySelectorAll('.skeleton')
+skeletons.forEach((skeleton) => {
+    setTimeout(() => {
+        $(skeletons).removeClass('skeleton')
+    }, 2000)
 })
 
 //dark/laght mode
@@ -20,26 +20,71 @@ let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
 }
 
 const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
 }
 
-if(darkmode === "active") enableDarkmode()
+if (darkmode === "active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
 
 //swiper category freemode
 var swiper2 = new Swiper('.swiperbox__categories', {
-  slidesPerView: 'auto',
-  freeMode: true,
-  rewind: false,
-  spaceBetween: 2
-}); 
+    slidesPerView: 'auto',
+    freeMode: true,
+    rewind: false,
+    spaceBetween: 10
+});
+
+var swiper2 = new Swiper('.swiperbox2__categories', {
+    slidesPerView: 'auto',
+    freeMode: true,
+    rewind: false,
+    spaceBetween: 10
+});
+
+//click ripple
+
+jQuery(document).ready(function ($) {
+
+    $('.btn-ripple').on('click', function (e) {
+
+        var $span = $('<span class="bc-ripple "></span>');
+        var $this = $(this);
+        $this.append($span);
+        var bcRipple = $this.find('.bc-ripple');
+        var bcOffset = $this.offset();
+        var pgY = e.pageY - bcOffset.top - (bcRipple.height() / 2); //- bcRipple.offset().top  ;
+        var pgX = e.pageX - bcOffset.left - (bcRipple.width() / 2); // - bcRipple.offset().left ;
+
+        bcRipple.css({
+            top: pgY + 'px',
+            left: pgX + 'px'
+        });
+
+        bcRipple.addClass('rp-active');
+        window.setTimeout(function () {
+            $span.remove();
+        }, 2000);
+    });
+});
+
+//fix window for load apps
+$(document).ready(function () {
+    $(".apparrow").on('click', function () {
+        $(".fixappbox").toggleClass("fixappbox2");
+        $(".apparrow i").toggleClass("apparrow2");
+        setTimeout(function () {
+            $(".apslink").toggleClass("apslink2");
+        }, 300);
+
+    });
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,21 @@
 
 <body>
     <x-AutoLocationUser></x-AutoLocationUser>
+    <div class="fixappbox df">
+        <div class="saopy df">
+            <img class="loadapp" src="https://i.postimg.cc/rmTBLp3j/scroll.png" alt="Loading app">
+            <h5>Ilovani yuklab oling.</h5>
+            <div class="apslink df">
+                <div class="poltre"><a href="#" class="and__link btn-ripple"><i class="fa-brands fa-android"></i>
+                        Android</a></div>
+                <div class="poltre"><a href="#" class="ios__link btn-ripple"><i class="fa-brands fa-apple"></i> IOS</a>
+                </div>
+            </div>
+        </div>
+        <div class="apparrow df">
+            <i class="fa-solid fa-caret-right"></i>
+        </div>
+    </div>
     <header>
         <div class="nav__wrap">
             <nav class="w70w">
@@ -28,53 +43,164 @@
                             <li><a href="#" class="navitems skeleton">Telefon</a></li>
                         </ul>
                         <button id="theme-switch" class="skeleton">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                            <svg class="switchsvg" xmlns="http://www.w3.org/2000/svg" height="24px"
+                                viewBox="0 -960 960 960" width="24px">
                                 <path
                                     d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" />
                             </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                            <svg class="switchsvg" xmlns="http://www.w3.org/2000/svg" height="24px"
+                                viewBox="0 -960 960 960" width="24px">
                                 <path
                                     d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z" />
                             </svg>
                         </button>
-                        <a href="#" class="elonberish skeleton"><i class="fa-solid fa-plus"></i> E'LON JOYLASH</a>
+                        <a href="#" class="elonberish skeleton">
+                            <svg version="1.1" id="fi_32563" class="eljoysvg" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="349.03px"
+                                height="349.031px" viewBox="0 0 349.03 349.031"
+                                style="enable-background:new 0 0 349.03 349.031;" xml:space="preserve">
+                                <g>
+                                    <path d="M349.03,141.226v66.579c0,5.012-4.061,9.079-9.079,9.079H216.884v123.067c0,5.019-4.067,9.079-9.079,9.079h-66.579
+		c-5.009,0-9.079-4.061-9.079-9.079V216.884H9.079c-5.016,0-9.079-4.067-9.079-9.079v-66.579c0-5.013,4.063-9.079,9.079-9.079
+		h123.068V9.079c0-5.018,4.069-9.079,9.079-9.079h66.579c5.012,0,9.079,4.061,9.079,9.079v123.068h123.067
+		C344.97,132.147,349.03,136.213,349.03,141.226z"></path>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                            </svg> E'LON JOYLASH</a>
                     </div>
                 </div>
             </nav>
         </div>
         <div class="markazbox w70w df">
-            <div class="protitle">
-                <h1>Ollbazar</h1>
-                <div class="prosubtitle">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolore molestias ipsam adipisci
-                    quibusdam laudantium assumenda molestiae totam distinctio esse. Id deserunt nisi magni.
-                </div>
-            </div>
             <div class="searchbox">
                 <div class="search__panel">
                     <div class="laerty df">
                         <div class="animation__word">Qidirmoq</div>
                         <div class="search__icon"><i class="fa-solid fa-magnifying-glass"></i></div>
                     </div>
+                    <a href="#" class="searchbymap">
+                        <img src="https://i.postimg.cc/L67fQggc/maps-and-location.png" alt="Search on map"
+                            class="searchbymapicon">
+                        <!-- <i class="fa-solid fa-map-location-dot"></i> -->
+                    </a>
                 </div>
             </div>
-            <div class="cats__box">
-                <div class="swiper-container swiperbox__categories">
-                    <ul class="swiper-wrapper categories">
-                        <li class="swiper-slide">
-                            <a href="#" class="c-link c-link-2" tabindex="0"><i class="fas fa-volleyball-ball"
-                                    aria-hidden="true"></i>Avtomobil</a>
-                        </li>
-                        <li class="swiper-slide df">
-                            <a href="">Item 7ghfddhgdhdfh</a>
-                        </li>
-                        <li class="swiper-slide df">
-                            <a href="" style="width:110px">Item 7ghfddhgdhdfh</a>
-                        </li>
-                        <li class="swiper-slide">
-                            <a href="">Item 21</a>
-                        </li>
-                    </ul>
+            <div class="yanabox df">
+                <div class="kytrew df">
+                    <a href="" class="ijaraavtolink"><img src="https://i.postimg.cc/BbbRS2z4/car-rental.png"
+                            alt="Ijara avto"> Ijara avto</a>
+                    <a href="" class="ijarauylink"><img src="https://i.postimg.cc/9FWY7WXw/apartments.png"
+                            alt="Ijara uy"> Ijara uy</a>
+                </div>
+            </div>
+            <div class="yanapot">
+                <div class="cats__box">
+                    <div class="swiper-container swiperbox__categories">
+                        <ul class="swiper-wrapper categories">
+                            <li class="swiper-slide">
+                                <a href="#" class="cat__item btn-ripple" tabindex="0">
+                                    <img src="https://i.postimg.cc/tC85Wvd6/spark.png" alt="avtomobil">
+                                    <span>Avtomobil</span>
+                                </a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="" class="cat__item btn-ripple">
+                                    <img class="uyjoyicon" src="https://i.postimg.cc/7LKmR3JM/uyjoy2.png" alt="uyjoy">
+                                    <span>Uy-joy</span>
+                                </a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="" class="cat__item btn-ripple">
+                                    <img class="ishboricon" src="https://i.postimg.cc/W1My5q7X/ishbor.png" alt="ishbor">
+                                    <span>Ish Bor</span>
+                                </a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="" class="cat__item btn-ripple">
+                                    <img class="telefonicon" src="https://i.postimg.cc/jSPjvc9m/telefon5.png"
+                                        alt="Telefon">
+                                    <span>Telefon</span>
+                                </a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="">sferfersfr</a>
+                            </li>
+                            <li class="swiper-slide">
+                                <a href="">hhhhhhhhhhhh</a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="">sferfersfr</a>
+                            </li>
+                            <li class="swiper-slide">
+                                <a href="">hhhhhhhhhhhh</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="swiper-container swiperbox2__categories">
+                        <ul class="swiper-wrapper categories">
+                            <li class="swiper-slide">
+                                <a href="#" class="cat__item btn-ripple" tabindex="0">
+                                    <img class="tehnikaicon" src="https://i.postimg.cc/zGF47Rz8/texnika-1.png"
+                                        alt="Tehnika">
+                                    <span>Texnika</span>
+                                </a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="#" class="cat__item btn-ripple">
+                                    <img class="hizmaticon" src="https://i.postimg.cc/J44T8dMV/hizmaticon.png"
+                                        alt="hizmatkorsatish">
+                                    <span>Hizmat ko'rsatish</span>
+                                </a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="#" class="cat__item btn-ripple">sferfersfr</a>
+                            </li>
+                            <li class="swiper-slide">
+                                <a href="#" class="cat__item btn-ripple">hhhhhhhhhhhh</a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="">sferfersfr</a>
+                            </li>
+                            <li class="swiper-slide">
+                                <a href="">hhhhhhhhhhhh</a>
+                            </li>
+                            <li class="swiper-slide df">
+                                <a href="">sferfersfr</a>
+                            </li>
+                            <li class="swiper-slide">
+                                <a href="">hhhhhhhhhhhh</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
     </header>
